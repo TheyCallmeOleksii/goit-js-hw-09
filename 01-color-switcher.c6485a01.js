cbbf041762a4474672c76e4a79d6c578743ea91d
TheyCallmeOleksii <127785349+TheyCallmeOleksii@users.noreply.github.com>
2023-08-25 05:55:50 +0000

    Deploying to gh-pages from @ TheyCallmeOleksii/goit-js-hw-09@147fef4c4a722966b265ac5f04d2b37110f5086e 🚀

---
+++ 01-color-switcher.c6485a01.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a||(a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(function(){a&&(clearInterval(a),a=null,t.disabled=!1,e.disabled=!0)}))}();
+//# sourceMappingURL=01-color-switcher.c6485a01.js.map
